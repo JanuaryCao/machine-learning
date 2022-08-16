1bd91872470ad2297b2825b165dd617bec909959
JanuaryCao <caoyang.shinny@gmail.com>
2022-08-17 00:03:19 +0900

    delete

--- data/download_images.sh
+++
@@ -1,7 +0,0 @@
-wget -O img0.jpg https://res.cloudinary.com/roundglass/image/upload/q_auto/ar_16:9,c_fill,w_1250/f_auto,g_auto/v1639561104/rg/collective/media/vyy8zc9ww3gqsjkqy9gk.jpg
-
-wget -O img1.jpg https://www.fundacion-affinity.org/sites/default/files/los-10-sonidos-principales-del-gato-fa.jpg
-
-wget -O img2.jpg https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*
-
-wget -O img3.jpg https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Parrot.jpg/1280px-Parrot.jpg
